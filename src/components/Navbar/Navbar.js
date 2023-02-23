121af0c40e3ef7afa3107725720b4b1269d62a0a
Juan Jose Caimen Cespedes <32856@pycomunicaciones.local>
2023-02-23 13:14:25 -0300

    commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,29 @@
 import classes from "./Navbar.module.css";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Reorder } from "@mui/icons-material";
 
 const Navbar = () => {
+  const [canExpandBar, setCanExpandBar] = useState(false);
+
+  useEffect(() => {
+    setCanExpandBar(false);
+  }, []);
+
+  const onChangeExpandBar = () => {
+    setCanExpandBar(!canExpandBar);
+  };
+
   return (
-    <div className={classes.Navbar}>
+    <div
+      className={classes.Navbar}
+      id={canExpandBar ? classes["open"] : classes["close"]}
+    >
+      <div className={classes.ToggleButton}>
+        <button onClick={onChangeExpandBar}>
+          <Reorder />
+        </button>
+      </div>
       <div className={classes.Links}>
         <Link to="/"> Home </Link>
         <Link to="/projects"> Projects </Link>
